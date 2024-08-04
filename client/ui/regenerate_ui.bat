cd7408281f5c726388946cdca33685c218ed8ab0
wevsty <ty@wevs.org>
2024-08-04 09:18:38 +0800

    update ui files

---
+++ client/ui/regenerate_ui.bat
@@ -0,0 +1,7 @@
+pyside6-uic controller_setup.ui -o controller_setup_ui.py
+pyside6-uic device_setup_dialog.ui -o device_setup_dialog_ui.py
+pyside6-uic indicator.ui -o indicator_ui.py
+pyside6-uic main.ui -o main_ui.py
+pyside6-uic numboard.ui -o numboard_ui.py
+pyside6-uic paste_board.ui -o paste_board_ui.py
+pyside6-uic shortcut_key.ui -o shortcut_key_ui.py
